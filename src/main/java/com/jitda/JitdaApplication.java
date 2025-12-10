3fa7df160d68b2380f5ad346f094c46b5690a087
MoonJaeGyeong <bblbblan69@gmail.com>
2025-12-10 20:42:33 +0900

    build : initial commit

---
+++ src/main/java/com/jitda/JitdaApplication.java
@@ -0,0 +1,13 @@
+package com.jitda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JitdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jitda/JitdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
